bb676cede1ca762b69e70de4402fff03e90d0358
Ravi-gurugram <125189351+Ravi-gurugram@users.noreply.github.com>
2025-08-02 12:33:43 +0530

    [976] [Time Beats: 76.67%] [Memory Beats: 45.74%] - LeetPush

---
+++ Largest-Perimeter-Triangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), greater<int>());  // Sort in descending order
+
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            if (nums[i] < nums[i + 1] + nums[i + 2]) {
+                // Valid triangle
+                return nums[i] + nums[i + 1] + nums[i + 2];
+            }
+        }
+
+        return 0;  // No valid triangle found
+    }
+};
